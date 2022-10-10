949c45b55326371deafbb3ea32f1c2bb6aa78443
Akibrot <akibrotsamuelas@gmail.com>
2022-10-10 19:55:39 +0300

    all set

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,11 @@ import express from "express";
 import {
   createProject,
   createTask,
+  createUser,
   deleteProject,
   getProjects,
   getTask,
+  getUsers,
   welcome,
 } from "../controllers/controllers.js";
 
@@ -17,5 +19,7 @@ router.post("/createProject", createProject);
 router.post("/createTask", createTask);
 router.post("/deleteProject", deleteProject);
 router.post("/getTask", getTask);
+router.post("/createUser", createUser);
+router.get("/getUsers", getUsers);
 
 export default router;
